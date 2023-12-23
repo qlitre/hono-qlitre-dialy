42066ed37193da031e78f77227b5e7abf45dc390
qlitre <kuri0403@gmail.com>
2023-12-23 14:51:17 +0900

    fixed siteSettings

--- src/settings/siteSettings.ts
+++ src/settings/siteSettings.ts
@@ -5,13 +5,13 @@ export const config = {
     // Headerなどに表示
     siteTitle: 'QLITRE DIALY',
     // スラッシュは最後にいれない
-    siteURL: 'https://hono-microcms-blog.qlitre.workers.dev',
+    siteURL: 'https://qlitre-dialy.ink',
     // footer、meta descriptionに表示
     author: 'qlitre',
     // meta descriptionに表示
     siteDescription: 'QLITREの日記サイト。',
     // HeaderのGitHUBリンク
-    repos: 'https://github.com/qlitre/hono-microcms-blog-sample',
+    repos: 'https://github.com/qlitre/hono-qlitre-dialy',
     // Aboutページのリンク
     about: 'https://qlitre.me',
     twitterURL: 'https://twitter.com/kuri_tter',
