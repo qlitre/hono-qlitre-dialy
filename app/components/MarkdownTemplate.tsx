31faf1e898c17dcdd1993cfcbe9234939fc07fec
qlitre <kuri0403@gmail.com>
2025-02-02 10:31:36 +0900

    microcms-rich-editor-handlerの利用

--- app/components/MarkdownTemplate.tsx
+++ app/components/MarkdownTemplate.tsx
@@ -1,14 +1,29 @@
 import { raw } from 'hono/html'
+import {
+    microCMSRichEditorHandler,
+    responsiveImageTransformer,
+    tocExtractor
+} from 'microcms-rich-editor-handler';
 
 type Props = {
     body: string
 }
 
 
-export const MarkdownTemplate = ({ body }: Props) => {
+export const MarkdownTemplate = async ({ body }: Props) => {
+    const { html, data } = await microCMSRichEditorHandler(
+        body, // MicroCMSから取得したデータのリッチエディタのHTML文字列
+        {
+            transformers: [responsiveImageTransformer()],
+            extractors: {
+                toc: [tocExtractor(), { phase: "before" }],
+            },
+        },
+    );
+
     return (
         <div class="md">
-            {raw(body)}
+            {raw(html)}
         </div>
     )
 }
