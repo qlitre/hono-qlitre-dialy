bf918be6b30b825a4d3fd0cb359d8c9cdcd3fdc0
qlitre <kuri0403@gmail.com>
2025-02-25 01:49:00 +0900

    マジックナンバーを変数化

--- app/routes/sitemap.xml.ts
+++ app/routes/sitemap.xml.ts
@@ -6,33 +6,34 @@ import { MicroCMSClient } from '../libs/microcmsClient'
 
 
 export default createRoute(async (c) => {
-    const client = new MicroCMSClient(c.env.SERVICE_DOMAIN, c.env.API_KEY)
-    const r = await client.getListResponse<MicroCMSListResponse<Post>>('post', { limit: 0 })
-    const tot = r.totalCount
-    const cnt = Math.ceil(tot / 50)
-    const posts: Post[] = []
-    for (let i = 0; i < cnt; i++) {
-        const offset = i * 50
-        const postList = await client.getListResponse<MicroCMSListResponse<Post>>('post', { limit: 50, offset: offset })
-        for (const post of postList.contents) {
-            posts.push(post)
-        }
+  const client = new MicroCMSClient(c.env.SERVICE_DOMAIN, c.env.API_KEY)
+  const r = await client.getListResponse<MicroCMSListResponse<Post>>('post', { limit: 0 })
+  const limit = 50
+  const tot = r.totalCount
+  const cnt = Math.ceil(tot / limit)
+  const posts: Post[] = []
+  for (let i = 0; i < cnt; i++) {
+    const offset = i * limit
+    const postList = await client.getListResponse<MicroCMSListResponse<Post>>('post', { limit: limit, offset: offset })
+    for (const post of postList.contents) {
+      posts.push(post)
     }
+  }
 
-    const baseUrl = config.siteURL
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  const baseUrl = config.siteURL
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>${baseUrl}/</loc>
     </url>
     ${posts.map(
-        (article) => `
+    (article) => `
     <url>
       <loc>${baseUrl}/post/${article.id}</loc>
       <lastmod>${article.updatedAt.split("T")[0]}</lastmod>
     </url>`
-    )
-            .join("")}
+  )
+      .join("")}
   </urlset>`;
-    return c.text(sitemap, 200, { "Content-Type": "application/xml" });
+  return c.text(sitemap, 200, { "Content-Type": "application/xml" });
 })
